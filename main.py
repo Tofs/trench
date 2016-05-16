5e67d371f223742d1f4e45602e217169d654c602
Anton Einarson <Anton.Einarson@gmail.com>
2016-05-16 22:50:23 +0200

    im a file now

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import pyglet
 
 window = pyglet.window.Window()
 
-label = pyglet.text.Label("hello world!",font_name='Times New Roman', font_size=36,
-                          x=window.width//2, y=window.height//2,
-                          anchor_x='center', anchor_y='center')
+missing = pyglet.resource.image(
+
 
 @window.event
 def on_draw():
 	window.clear()
-	label.draw()
+	image.draw()
 
 pyglet.app.run()
