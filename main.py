f219b8e06c51690fde9a01da64930a68557dfe64
Anton Einarson <Anton.Einarson@gmail.com>
2016-05-16 22:53:19 +0200

    now we have an image instead of an text woho!

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import pyglet
 
 window = pyglet.window.Window()
 
-missing = pyglet.resource.image(
+image = pyglet.resource.image("resources/images/resourceNotFound.png")
+print(image)
 
 
 @window.event
 def on_draw():
 	window.clear()
-	image.draw()
+	image.blit(10, 10)
 
 pyglet.app.run()
